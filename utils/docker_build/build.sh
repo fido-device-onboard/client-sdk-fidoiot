79eb9deba28e2825a879dc226a5574f723a11938
Davis Benny <davis.benny@intel.com>
2020-08-06 17:49:24 +0530

    Moving all dependency installation to Dockerfile
       i) Moving all dependency installation to Dockerfile. So that
       build scripts are only executed during container run-time.
    
       ii) Docker build folder moved into utils
    
    Signed-off-by: Davis Benny <davis.benny@intel.com>

--- utils/docker_build/build.sh
+++ utils/docker_build/build.sh
@@ -6,24 +6,7 @@
 REMOTE_URL=https://github.com/secure-device-onboard/client-sdk.git
 REMOTE_BRANCH=master
 
-export DEBIAN_FRONTEND=noninteractive
-
-apt-get install -y build-essential
-apt-get install -y cmake
-apt-get install -y libssl-dev
-apt-get install -y ruby
-apt-get install -y wget
-apt-get install -y tar
-apt-get install -y unzip
-apt-get install -y python
-apt-get install -y python-pip
-
-git clone https://github.com/intel/safestringlib.git
-cd safestringlib/
-make
 export SAFESTRING_ROOT=/home/sdouser/client-sdk/safestringlib
-cd ..
-rm CMakeCache.txt
 
 if [ "$use_remote" = "1" ]; then
   echo "Building $REMOTE_URL : $REMOTE_BRANCH"
@@ -31,15 +14,10 @@ if [ "$use_remote" = "1" ]; then
   git clone $REMOTE_URL
   cd /tmp/client-sdk/
   git checkout $REMOTE_BRANCH
+
   ./cDevice_Build.sh
-  cd utils/
-  sed 's/sudo//g' install_tpm_libs.sh > script.sh
-  chmod 777 script.sh
-  unset DEBIAN_FRONTEND
-  yes | ./script.sh -i
-  yes | ./script.sh -t
-  cd ..
   ./cDevice_Build_tpm.sh
+
   # Copying Binaries back to the mounted volume.
   cp -r /tmp/client-sdk/ecdsa256_c_device_bin/ /home/sdouser/client-sdk/
   cp -r /tmp/client-sdk/ecdsa256_c_sct_device_bin/ /home/sdouser/client-sdk/
@@ -48,13 +26,6 @@ if [ "$use_remote" = "1" ]; then
 
 else
   ./cDevice_Build.sh
-  cd utils/
-  sed 's/sudo//g' install_tpm_libs.sh > script.sh
-  chmod 777 script.sh
-  unset DEBIAN_FRONTEND
-  yes | ./script.sh -i
-  yes | ./script.sh -t
-  cd ..
   ./cDevice_Build_tpm.sh
 fi
 
