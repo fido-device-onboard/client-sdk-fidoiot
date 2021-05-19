b20832b2a0f3f7cf30816a3357df52104b4b5cce
Behera, Tushar Ranjan <tushar.ranjan.behera@intel.com>
2021-05-19 22:39:22 +0530

    Cleanup for test code
    Also change the TO0 scheduler time to 30sec for faster execution of
    smoketest.
    
    Signed-off-by: Behera, Tushar Ranjan <tushar.ranjan.behera@intel.com>

--- cDevice_Build.sh
+++ cDevice_Build.sh
@@ -36,26 +36,27 @@ function copy_build_artifacts()
 }
 
 ## Common build configurations
-DEFAULT_CONFIG="-DBUILD=${BUILDTYPE} -DMANUFACTURER_TOOLKIT=true -DMODULES=true -DPK_ENC=ecdsa"
+nproc=$(cat /proc/cpuinfo | grep processor | wc -l)
+COMMON_BUILD_CONFIG="-DBUILD=${BUILDTYPE} -DMANUFACTURER_TOOLKIT=true -DMODULES=true -DPK_ENC=ecdsa"
 
 ## ECDSA256 build configurations
 echo "***** Building with ECDSA256 support *****"
 make pristine || true
-cmake ${DEFAULT_CONFIG} -DDA=ecdsa256 -DKEX=ecdh .
+cmake ${COMMON_BUILD_CONFIG} -DDA=ecdsa256 -DKEX=ecdh .
 make -j$(nproc)
 copy_build_artifacts x86_ecdsa256_bin
 
 # Build configurations for ECDSA384
 echo "***** Building with ECDSA384 support *****"
 make pristine || true
-cmake ${DEFAULT_CONFIG} -DDA=ecdsa384 -DKEX=ecdh384 .
+cmake ${COMMON_BUILD_CONFIG} -DDA=ecdsa384 -DKEX=ecdh384 .
 make -j$(nproc)
 copy_build_artifacts x86_ecdsa384_bin
 
 # Build configurations for TPM based devices
 echo "***** Building with TPM (ECDSA256) support *****"
 make pristine || true
-cmake ${DEFAULT_CONFIG} -DDA=tpm20_ecdsa256 -DPK_ENC=ecdsa .
+cmake ${COMMON_BUILD_CONFIG} -DDA=tpm20_ecdsa256 -DPK_ENC=ecdsa .
 make -j$(nproc)
 copy_build_artifacts tpm_ecdsa256_bin
 
@@ -66,11 +67,21 @@ copy_build_artifacts tpm_ecdsa256_bin
 echo " ***** Running Unit Tests *****"
 TEST_OUTPUT="build/unit-test-output.txt"
 rm -f $TEST_OUTPUT
-make pristine || true; cmake -Dunit-test=true -DHTTPPROXY=true -DBUILD=release -DKEX=ecdh -DAES_MODE=ctr -DDA=ecdsa256 -DPK_ENC=ecdsa ; make -j$(nproc) | tee -a $TEST_OUTPUT
-make pristine || true; cmake -Dunit-test=true -DHTTPPROXY=true -DBUILD=release -DKEX=ecdh -DAES_MODE=cbc -DDA=ecdsa256 -DPK_ENC=ecdsa ; make -j$(nproc) | tee -a $TEST_OUTPUT
-make pristine || true; cmake -Dunit-test=true -DHTTPPROXY=true -DBUILD=release -DKEX=ecdh384 -DAES_MODE=ctr -DDA=ecdsa384 -DPK_ENC=ecdsa ; make -j$(nproc) | tee -a $TEST_OUTPUT
-make pristine || true; cmake -Dunit-test=true -DHTTPPROXY=true -DBUILD=release -DKEX=ecdh384 -DAES_MODE=cbc -DDA=ecdsa384 -DPK_ENC=ecdsa ; make -j$(nproc) | tee -a $TEST_OUTPUT
-make pristine || true; cmake -Dunit-test=true -DHTTPPROXY=true -DBUILD=release -DKEX=ecdh -DAES_MODE=ctr -DDA=ecdsa256 -DPK_ENC=ecdsa -DDA_FILE=pem ; make -j$(nproc) | tee -a $TEST_OUTPUT
+COMMON_TEST_CONFIG="-Dunit-test=true -DHTTPPROXY=true -DBUILD=release -DPK_ENC=ecdsa"
+
+# Run unit-test with given test configuration
+function run_unit_test
+{
+  make pristine || true
+  cmake ${COMMON_TEST_CONFIG} $@
+  make -j$(nproc) | tee -a $TEST_OUTPUT
+}
+
+run_unit_test -DDA=ecdsa256 -DKEX=ecdh -DAES_MODE=ctr
+run_unit_test -DDA=ecdsa256 -DKEX=ecdh -DAES_MODE=cbc
+run_unit_test -DDA=ecdsa384 -DKEX=ecdh384 -DAES_MODE=ctr
+run_unit_test -DDA=ecdsa384 -DKEX=ecdh384 -DAES_MODE=cbc
+run_unit_test -DDA=ecdsa256 -DKEX=ecdh -DAES_MODE=ctr -DDA_FILE=pem
 
 fail_count=$(awk '/Tests Failed  :/ {split($0,a,": "); count+=a[2]} END{print count}' $TEST_OUTPUT)
 echo "Found $fail_count unit-test failure(s)."
