3cb148603963c76c24775f0f0005356fcedcfa84
Behera, Tushar Ranjan <tushar.ranjan.behera@intel.com>
2021-07-02 15:17:46 +0530

    Further cleanup
    -typedef enum { KEXSUITE_NONE, DHKEXid14, ECDH } key_ex_suite_t;
    -typedef enum { HMAC_NONE, HMAC_SHA256, HMAC_SHA384 } hmac_name_t;
    -typedef enum { HASH_SHA256, HASH_SHA384 } hash_type_t;
    -typedef enum { NIST_P_256, NIST_P_384 } ecccurve_type_t;
    -typedef enum { RFC3526_P2048, RFC3526_P3072 } modp_group_ke_t;
    
    Signed-off-by: Behera, Tushar Ranjan <tushar.ranjan.behera@intel.com>

--- lib/include/fdokeyexchange.h
+++ lib/include/fdokeyexchange.h
@@ -9,12 +9,6 @@
 #include "fdotypes.h"
 #include <stdbool.h>
 
-typedef enum { KEXSUITE_NONE, DHKEXid14, ECDH } key_ex_suite_t;
-typedef enum { HMAC_NONE, HMAC_SHA256, HMAC_SHA384 } hmac_name_t;
-typedef enum { HASH_SHA256, HASH_SHA384 } hash_type_t;
-typedef enum { NIST_P_256, NIST_P_384 } ecccurve_type_t;
-typedef enum { RFC3526_P2048, RFC3526_P3072 } modp_group_ke_t;
-
 int32_t fdo_set_kex_paramA(fdo_byte_array_t *xA, fdo_public_key_t *encrypt_key);
 int32_t fdo_get_kex_paramB(fdo_byte_array_t **xB);
 fdo_string_t *get_kex_name(void);
