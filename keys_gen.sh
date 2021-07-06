728c6a40b9bfd2f2d68040fd1477ada7cfc5fbed
Behera, Tushar Ranjan <tushar.ranjan.behera@intel.com>
2021-07-06 10:58:09 +0530

    Log the key files generated
    Signed-off-by: Behera, Tushar Ranjan <tushar.ranjan.behera@intel.com>

--- keys_gen.sh
+++ keys_gen.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
-shopt -s extglob
-set -e
-bold=$(tput bold)
-normal=$(tput sgr0)
 
 CSDK_REPO=$1
 CSDK_DATA=$CSDK_REPO/data
 
+EC256_DAT=$CSDK_DATA/ecdsa256privkey.dat
+EC256_PEM=$CSDK_DATA/ecdsa256privkey.pem
+EC384_DAT=$CSDK_DATA/ecdsa384privkey.dat
+EC384_PEM=$CSDK_DATA/ecdsa384privkey.pem
+
 usage()
 {
-	echo -e "Usage:
-        $0  <absolute_path_of_CSDK_repo>\n"
+  echo -e "Usage:
+    $0 <absolute_path_of_CSDK_repo>\n"
 }
 
-csdk_keys()
+gen_csdk_keys()
 {
-	# Generation of ECDSA 256 and 384 .pem files and .dat files
-	openssl ecparam -name prime256v1 -genkey -noout -out $CSDK_DATA/ecdsa256privkey.pem
-	echo `openssl asn1parse < $CSDK_DATA/ecdsa256privkey.pem | grep "HEX DUMP" |\
-		cut -d ":" -f 4` | xxd -r -p > $CSDK_DATA/ecdsa256privkey.dat
-	openssl ecparam -name secp384r1 -genkey -noout -out $CSDK_DATA/ecdsa384privkey.pem
-	echo `openssl asn1parse < $CSDK_DATA/ecdsa384privkey.pem | grep "HEX DUMP" |\
-		cut -d ":" -f 4` | xxd -r -p > $CSDK_DATA/ecdsa384privkey.dat
-	echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	echo "################### CSDK PEM files Generated Successfully ####################"
-	echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	git status $CSDK_REPO
+  # Generation of ECDSA 256 and 384 .pem files and .dat files
+  openssl ecparam -name prime256v1 -genkey -noout -out $EC256_PEM
+  echo "Generated $EC256_PEM"
+
+  echo `openssl asn1parse < $EC256_PEM | grep "HEX DUMP" |\
+    cut -d ":" -f 4` | xxd -r -p > $EC256_DAT
+  echo "Generated $EC256_DAT"
+
+  openssl ecparam -name secp384r1 -genkey -noout -out $EC384_PEM
+  echo "Generated $EC384_PEM"
+
+  echo `openssl asn1parse < $EC384_PEM | grep "HEX DUMP" |\
+    cut -d ":" -f 4` | xxd -r -p > $EC384_DAT
+  echo "Generated $EC384_DAT"
 }
 
 if [[ $# == 1 ]]; then
-	csdk_keys
+  gen_csdk_keys
 else
-	usage; exit 1;
+  usage; exit 1;
 fi
