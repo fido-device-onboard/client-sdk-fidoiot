936a82483fd25cb5a8d4a71f626d96b31a31f79b
Shrikant Temburwar <shrikant.temburwar@intel.com>
2024-01-11 22:33:33 +0530

    Add execution logs in clear_tpm_nv.sh
    Signed-off-by: Shrikant Temburwar <shrikant.temburwar@intel.com>

--- utils/clear_tpm_nv.sh
+++ utils/clear_tpm_nv.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
+execute_cmd_on_failure_exit()
+{
+    eval exec_cmd="$1"
+    eval success_msg="$2"
+    eval failure_msg="$3"
+
+    echo -e "\e[2;33mExecuting :\e[0m ${exec_cmd}"
+    out=$(eval ${exec_cmd}" 2>&1")
+    if [ `echo $?` != 0 ]; then
+        echo -e "\e[2;31m${failure_msg}\e[0m"
+    else
+        echo -e "\e[2;32m${success_msg}\e[0m"
+    fi
+
+    return 0
+}
+
 for n in {1..9};
 do
-    tpm2_nvundefine $n
+    task="Deleting a Non-Volatile (NV) index at 0x100000$n"
+    cmd="tpm2_nvundefine 0x100000$n"
+    success_string="$task completed successfully!!"
+    failure_string="Non-Volatile (NV) index at 0x100000$n is not defined!!"
+    execute_cmd_on_failure_exit "\$cmd" "\$success_string" "\$failure_string"
 done
 echo "TPM NV storage cleared!"
